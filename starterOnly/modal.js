accf63f57c0ff5776fb3eeba1d5c9a05e76f1eb2
ribeau <guillaume>
2021-03-20 15:06:05 +0100

    no message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,9 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn= document.querySelector(".close");
+const inscriptionValid=document.getElementById("inscription_valid");
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -25,5 +28,52 @@ function launchModal() {
 function closeModal(){
   modalbg.style.display='none';
 }
+
+
+// message de validation du formulaire
+
+inscriptionValid.addEventListener('submit',function(e){
+  e.preventDefault();
+  alert('Bravo le formulaire à bien été envoyé !')
+  
+})
+
+
+
+//creation de span pour les div class=dataform
+const createSpan = document.createElement ('span');
+const divDataForm = document.querySelector(".formData");
+divDataForm.appendChild(createSpan);
+
+
+//regex prenom 
+let prenomValid = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêëçîôï] + ([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêëçîôï]+)/;
+
+// verification du champ prenom
+
+const prenom = document.getElementById ('first'); 
+let btnSubmit = document.getElementById('submit_valid');
+
+
+
+btnSubmit.addEventListener('click',prenom_v);
+
+function prenom_v(e) {
+  if (prenom.validity.valueMissing) {
+    e.preventDefault();
+    let contentSpanVide= document.createTextNode ('Le champ prénom est requis');
+    createSpan.appendChild(contentSpanVide);
+    createSpan.style.color='red';
+    createSpan.style.fontSize='14px'; 
+    
+  }
+  else if (prenomValid.test(prenom.value)==false){
+    e.preventDefault();
+    let contentSpanError = document.createTextNode ('le prénom doit contenir au moins deux caractères');
+    createSpan.appendChild(contentSpanError);
+
+  }
+}
+
 
 
